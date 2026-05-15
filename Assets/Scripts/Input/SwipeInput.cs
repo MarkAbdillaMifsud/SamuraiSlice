6cf7b0fd394735f6a43172e44c431f5703a3bf81
Mark Abdilla Mifsud <60700422+MarkAbdillaMifsud@users.noreply.github.com>
2026-05-15 18:12:50 +0200

    swipe path and debug visuals

--- Assets/Scripts/Input/SwipeInput.cs
+++ Assets/Scripts/Input/SwipeInput.cs
@@ -2,122 +2,125 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class SwipeInput : MonoBehaviour
+namespace SamuraiSlice
 {
-    [Header("Input")]
-    [SerializeField] private InputActionAsset playerInputActions;
-    [SerializeField] private string actionMapName = "Gameplay";
-    [SerializeField] private string pressActionName = "PointerPress";
-    [SerializeField] private string positionActionName = "Pointer";
-
-    [Header("Camera")]
-    [SerializeField] private Camera worldCamera;
-
-    [Header("Sampling")]
-    [SerializeField] private float minSampleDistance = 0.02f;
-
-    private InputAction _pressAction;
-    private InputAction _positionAction;
-
-    private Vector2 _lastWorldPoint;
-    private Vector2 _currentWorldPoint;
-    private bool _hasFirstSample;
-
-    public bool IsSwiping { get; private set; }
-    public Vector2 CurrentWorldPoint => CurrentWorldPoint;
-
-    // Will be called by SwipeDetector to decide whether to run the Linecast or not 
-    public bool TryGetCurrentSegment(out Vector2 from, out Vector2 to)
+    public class SwipeInput : MonoBehaviour
     {
-        if(IsSwiping && _hasFirstSample)
-        {
-            from = _lastWorldPoint;
-            to = _currentWorldPoint;
-            return from != to;
-        }
-        from = to = default;
-        return false;
-    }
+        [Header("Input")]
+        [SerializeField] private InputActionAsset playerInputActions;
+        [SerializeField] private string actionMapName = "Gameplay";
+        [SerializeField] private string pressActionName = "PointerPress";
+        [SerializeField] private string positionActionName = "Pointer";
 
-    private void Awake()
-    {
-        if (worldCamera == null)
+        [Header("Camera")]
+        [SerializeField] private Camera worldCamera;
+
+        [Header("Sampling")]
+        [SerializeField] private float minSampleDistance = 0.02f;
+
+        private InputAction _pressAction;
+        private InputAction _positionAction;
+
+        private Vector2 _lastWorldPoint;
+        private Vector2 _currentWorldPoint;
+        private bool _hasFirstSample;
+
+        public bool IsSwiping { get; private set; }
+        public Vector2 CurrentWorldPoint => _currentWorldPoint;
+
+        // Will be called by SwipeDetector to decide whether to run the Linecast or not 
+        public bool TryGetCurrentSegment(out Vector2 from, out Vector2 to)
         {
-            worldCamera = Camera.main;
+            if (IsSwiping && _hasFirstSample)
+            {
+                from = _lastWorldPoint;
+                to = _currentWorldPoint;
+                return from != to;
+            }
+            from = to = default;
+            return false;
         }
 
-        // Ensures immediate error if action map is renamed in the asset but not in this script's relevant fields
-        var map = playerInputActions.FindActionMap(actionMapName, throwIfNotFound: true);
-        _pressAction = map.FindAction(pressActionName, throwIfNotFound: true);
-        _positionAction = map.FindAction(positionActionName, throwIfNotFound: true);
-    }
-
-    private void OnEnable()
-    {
-        playerInputActions.Enable();
-        _pressAction.started += HandlePressStarted;
-        _pressAction.canceled += HandlePressEnded;
-    }
-
-    private void OnDisable()
-    {
-        _pressAction.started -= HandlePressStarted;
-        _pressAction.canceled -= HandlePressEnded;
-        playerInputActions.Disable();
-        ResetSwipe();
-    }
-
-    private void Update()
-    {
-        if (!IsSwiping)
+        private void Awake()
         {
-            return;
+            if (worldCamera == null)
+            {
+                worldCamera = Camera.main;
+            }
+
+            // Ensures immediate error if action map is renamed in the asset but not in this script's relevant fields
+            var map = playerInputActions.FindActionMap(actionMapName, throwIfNotFound: true);
+            _pressAction = map.FindAction(pressActionName, throwIfNotFound: true);
+            _positionAction = map.FindAction(positionActionName, throwIfNotFound: true);
         }
 
-        Vector2 sampled = ScreenToWorld(_positionAction.ReadValue<Vector2>());
-
-        if(!_hasFirstSample)
+        private void OnEnable()
         {
-            _lastWorldPoint = sampled;
-            _currentWorldPoint = sampled;
-            _hasFirstSample = true;
-            return;
+            playerInputActions.Enable();
+            _pressAction.started += HandlePressStarted;
+            _pressAction.canceled += HandlePressEnded;
         }
 
-        // Ensure a swipe is valid first rather than processing everything every frame
-        if(Vector2.Distance(sampled, _currentWorldPoint) >= minSampleDistance)
+        private void OnDisable()
         {
-            _lastWorldPoint = _currentWorldPoint;
-            _currentWorldPoint = sampled;
+            _pressAction.started -= HandlePressStarted;
+            _pressAction.canceled -= HandlePressEnded;
+            playerInputActions.Disable();
+            ResetSwipe();
         }
-    }
 
-    private void HandlePressStarted(InputAction.CallbackContext _)
-    {
-        IsSwiping = true;
-        _hasFirstSample = false;
-    }
+        private void Update()
+        {
+            if (!IsSwiping)
+            {
+                return;
+            }
 
-    private void HandlePressEnded(InputAction.CallbackContext _) => ResetSwipe();
+            Vector2 sampled = ScreenToWorld(_positionAction.ReadValue<Vector2>());
 
-    private void ResetSwipe()
-    {
-        IsSwiping = false;
-        _hasFirstSample = false;
-    }
+            if (!_hasFirstSample)
+            {
+                _lastWorldPoint = sampled;
+                _currentWorldPoint = sampled;
+                _hasFirstSample = true;
+                return;
+            }
 
-    private Vector2 ScreenToWorld(Vector2 screenPoint)
-    {
-        Vector3 sp = new(screenPoint.x, screenPoint.y, -worldCamera.transform.position.z);
-        return worldCamera.ScreenToWorldPoint(sp);
-    }
+            // Ensure a swipe is valid first rather than processing everything every frame
+            if (Vector2.Distance(sampled, _currentWorldPoint) >= minSampleDistance)
+            {
+                _lastWorldPoint = _currentWorldPoint;
+                _currentWorldPoint = sampled;
+            }
+        }
 
-    //TODO: Remove later - intended for gizmo testing of swiping
-    private void OnDrawGizmos()
-    {
-        if (!IsSwiping || !_hasFirstSample) return;
-        Gizmos.color = Color.cyan;
-        Gizmos.DrawLine(_lastWorldPoint, _currentWorldPoint);
-        Gizmos.DrawSphere(_currentWorldPoint, 0.1f);
+        private void HandlePressStarted(InputAction.CallbackContext _)
+        {
+            IsSwiping = true;
+            _hasFirstSample = false;
+        }
+
+        private void HandlePressEnded(InputAction.CallbackContext _) => ResetSwipe();
+
+        private void ResetSwipe()
+        {
+            IsSwiping = false;
+            _hasFirstSample = false;
+        }
+
+        private Vector2 ScreenToWorld(Vector2 screenPoint)
+        {
+            Vector3 sp = new(screenPoint.x, screenPoint.y, -worldCamera.transform.position.z);
+            return worldCamera.ScreenToWorldPoint(sp);
+        }
+
+        //TODO: Remove later - intended for gizmo testing of swiping
+        private void OnDrawGizmos()
+        {
+            if (!IsSwiping || !_hasFirstSample) return;
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(_lastWorldPoint, _currentWorldPoint);
+            Gizmos.DrawSphere(_currentWorldPoint, 0.1f);
+        }
     }
 }
---
+++ Assets/Scripts/Input/SwipeTracker.cs
@@ -0,0 +1,77 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace SamuraiSlice
+{
+    public class SwipeTracker : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private SwipeInput swipeInput;
+
+        [Header("Buffer")]
+        [SerializeField, Min(2)] private int maxSamples = 8;
+
+        [SerializeField] private float clearGrace = 0.1f;
+
+        [Header("Debug")]
+        [SerializeField] private bool drawDebug = true;
+        [SerializeField] private Color debugColour = Color.cyan;
+
+        private readonly List<Vector2> _path = new();
+        private float _releaseTime = -1.0f;
+
+        public IReadOnlyList<Vector2> CurrentPath => _path; //readonly view of the current swipe path (empty when player is not swiping)
+
+        private void Awake()
+        {
+            if(swipeInput == null)
+            {
+                swipeInput = GetComponent<SwipeInput>();
+            }
+            if(swipeInput == null) //runs if GetComponent returns false
+            {
+                Debug.LogError($"{nameof(SwipeTracker)} requires a {nameof(SwipeInput)} reference.", this);
+                enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            if(swipeInput.IsSwiping)
+            {
+                _releaseTime = -1f;
+                AppendSample(swipeInput.CurrentWorldPoint);
+            } else if (_path.Count > 0)
+            {
+                if(_releaseTime < 0f)
+                {
+                    _releaseTime = Time.time;
+                }
+                else if (Time.time - _releaseTime >= clearGrace)
+                {
+                    _path.Clear();
+                    _releaseTime = -1f;
+                }
+            }
+
+            if (drawDebug)
+            {
+                for(int i = 1; i < _path.Count; i++)
+                {
+                    Debug.DrawLine(_path[i - 1], _path[i], debugColour);
+                }
+            }
+        }
+
+        private void AppendSample(Vector2 sample)
+        {
+            _path.Add(sample);
+            if(_path.Count > maxSamples)
+            {
+                _path.RemoveAt(0);
+            }
+        }
+    }
+}
