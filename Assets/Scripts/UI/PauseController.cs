ce7ac5b27c6b79cf4bb552fc7be33f524be14464
Mark Abdilla Mifsud <markabdilla26@gmail.com>
2026-06-02 13:02:28 +0200

    Fixed bug allowing slicing while paused

--- Assets/Scripts/UI/PauseController.cs
+++ Assets/Scripts/UI/PauseController.cs
@@ -1,27 +1,76 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace SamuraiSlice
 {
     public class PauseController : MonoBehaviour
     {
         [SerializeField] private GameObject pausePanel;
+        [SerializeField] private Button pauseButton;
+        [SerializeField] private SliceDetector sliceDetector;
+
+        private void Start()
+        {
+            SetPanelVisible(false);
+
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.OnStateChanged += HandleStateChanged;
+            } else
+            {
+                Debug.LogError("[PauseController] GameManager.Instance is null in Start. Ensure GameManager is in the scene and executes before this component.", this);
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (GameManager.Instance != null)
+            {
+                GameManager.Instance.OnStateChanged -= HandleStateChanged;
+            }
+        }
 
         public void Pause()
         {
-            GameManager.Instance.Pause();
-            pausePanel.SetActive(true);
+            GameManager.Instance?.Pause();
         }
 
         public void Resume()
         {
-            GameManager.Instance.Resume();
-            pausePanel.SetActive(false);
+            GameManager.Instance?.Resume();
         }
 
         public void ReturnToMainMenu()
         {
+            Time.timeScale = 1f;
             SceneManager.LoadScene("MainMenu");
+        }
+
+        private void HandleStateChanged(GameManager.GameState newState)
+        {
+            bool isPaused = newState == GameManager.GameState.Paused;
+            bool isPlaying = newState == GameManager.GameState.Playing;
+
+            SetPanelVisible(isPaused);
+
+            if(sliceDetector != null)
+            {
+                sliceDetector.enabled = isPlaying;
+            }
+
+            if(pauseButton != null)
+            {
+                pauseButton.gameObject.SetActive(isPlaying);
+            }
+        }
+
+        private void SetPanelVisible(bool visible)
+        {
+            if(pausePanel != null)
+            {
+                pausePanel.SetActive(visible);
+            }
         }
     }
 }
