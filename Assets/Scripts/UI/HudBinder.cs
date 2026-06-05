d8fdbe738c5f18f7bbbf41349c7bc2da00805cda
Mark Abdilla Mifsud <markabdilla26@gmail.com>
2026-06-05 17:21:16 +0200

    Added combo tracker to HUD and fixed background sorting

--- Assets/Scripts/UI/HudBinder.cs
+++ Assets/Scripts/UI/HudBinder.cs
@@ -7,8 +7,10 @@ namespace SamuraiSlice
     {
         [SerializeField] private ScoreManager scoreManager;
         [SerializeField] private MissCounter missCounter;
+        [SerializeField] private ComboTracker comboTracker;
         [SerializeField] TMP_Text scoreText;
         [SerializeField] TMP_Text missText;
+        [SerializeField] TMP_Text comboText;
 
         const int MaxMisses = 3;
 
@@ -24,17 +26,26 @@ namespace SamuraiSlice
                 missCounter.OnMissCountChanged += HandleMissCountChanged;
                 HandleMissCountChanged(missCounter.CurrentMisses);
             }
+            if (comboTracker != null)
+            {
+                comboTracker.OnMultiplierChanged += HandleComboChanged;
+                HandleComboChanged(comboTracker.CurrentMultiplier);
+            }
         }
 
         private void OnDisable()
         {
-            if(scoreManager != null)
+            if (scoreManager != null)
             {
                 scoreManager.OnScoreChanged -= HandleScoreChanged;
             }
             if (missCounter != null)
             {
                 missCounter.OnMissCountChanged -= HandleMissCountChanged;
+            }
+            if (comboTracker != null)
+            {
+                comboTracker.OnMultiplierChanged -= HandleComboChanged;
             }
         }
 
@@ -51,6 +62,14 @@ namespace SamuraiSlice
             if(missText != null)
             {
                 missText.text = $"{newMisses}/{MaxMisses}";
+            }
+        }
+
+        private void HandleComboChanged(int newMultiplier)
+        {
+            if(comboText != null)
+            {
+                comboText.text = newMultiplier <= 1 ? string.Empty : $"x{newMultiplier}";
             }
         }
     }
